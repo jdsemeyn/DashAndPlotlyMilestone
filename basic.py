ccd9090dde5a53e95d5a67b3ded615b7ccb245c5
jdsemeyn <jdsemeyn@outlook.com>
2021-06-29 16:52:05 -0500

    added dropdown menu and multi choice functionality for symbol

--- basic.py
+++ basic.py
@@ -4,17 +4,28 @@ import dash_html_components as html
 from dash.dependencies import Input,Output,State
 import pandas_datareader.data as web
 from datetime import datetime
+import pandas as pd
 
 app = dash.Dash()
+
+nsdq = pd.read_csv('data/NASDAQcompanylist.csv',index_col='Symbol')
+options = []
+
+for symb in nsdq.index:
+    symDict = {}
+    symDict['label'] = str(nsdq.loc[symb]['Name']) + ' ' + symb
+    symDict['value'] = symb
+    options.append(symDict)
 
 app.layout = html.Div([
             html.H1('Stock Ticker Dashboard'),
             html.Div([html.H3('Enter a stock symbol:',
                     style={'paddingRight':'30px'}),
-            dcc.Input(id='my_stock_picker',
-                    value='NVDA',
-                    style={'fontSize':24,'width':75}
-            )],style={'display':'inline-block','verticalAlign':'top'}),
+            dcc.Dropdown(id='my_stock_picker',
+                    options=options,
+                    value=['NVDA'],
+                    multi=True
+            )],style={'display':'inline-block','verticalAlign':'top','width':'30%'}),
             html.Div([html.H3('Select a start and end date:'),
                     dcc.DatePickerRange(id='my_date_picker',
                                         min_date_allowed=datetime(2010,1,1),
@@ -46,8 +57,12 @@ app.layout = html.Div([
 def update_graph(n_clicks,stock_ticker,start_date,end_date):
     start = datetime.strptime(start_date[:10],'%Y-%m-%d')
     end = datetime.strptime(end_date[:10],'%Y-%m-%d')
-    df = web.DataReader(stock_ticker,'stooq',start,end)
-    fig = {'data':[{'x':df.index,'y':df['Close']}],
+
+    traces = []
+    for symb in stock_ticker:
+        df = web.DataReader(symb,'stooq',start,end)
+        traces.append({'x':df.index,'y':df['Close'],'name':symb})
+    fig = {'data':traces,
         'layout':{'title':stock_ticker}
     }
     return fig
