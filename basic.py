45470b46d7ff4da1571a15f9dbf589115ee54437
jdsemeyn <jdsemeyn@outlook.com>
2021-06-29 15:22:23 -0500

    added function to update plot title

--- basic.py
+++ basic.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input,Output,State
 
 app = dash.Dash()
 
@@ -13,10 +14,21 @@ app.layout = html.Div([
             dcc.Graph(id='my_graph',
                     figure={'data':[
                             {'x':[1,2],'y':[3,1]}
-                            ]
+                            ],
+                            'layout':{
+                            'title':'Default Title'
+                            }
                     }
                 )
             ])
+
+@app.callback(Output('my_graph','figure'),
+            [Input('my_stock_picker','value')])
+def update_graph(stock_ticker):
+    fig = {'data':[{'x':[1,2],'y':[3,1]}],
+        'layout':{'title':stock_ticker}
+    }
+    return fig
 
 if __name__=='__main__':
     app.run_server()
